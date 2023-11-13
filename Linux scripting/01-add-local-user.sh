8d857980f1022bc4598d85ed3c4e13624d1d9b67
jakcloudlearning <150512232+jakcloudlearning@users.noreply.github.com>
2023-11-13 18:51:34 +0100

    Update 01-add-local-user.sh

--- Linux scripting/01-add-local-user.sh
+++ Linux scripting/01-add-local-user.sh
@@ -20,8 +20,8 @@ then
    echo "You can not create user with the following username: ${USER_NAME}"
    exit 1
 fi
-
-PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c10)
+SPECIAL_CHAR=$(echo '!@#$%^&*)(_+=-' | fold -w1 | shuf | head -c1)
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c10)${SPECIAL_CHAR}
 echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 if [[ ${?} -ne 0 ]]
 then
