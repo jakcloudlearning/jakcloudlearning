c9220bbeb767f3e894ab245a49a650d40eddd9a7
jakcloudlearning <jakub.cloud.learning@gmail.com>
2023-11-23 19:20:31 +0100

    Update 01-add-local-user.sh

--- Linux scripting/01-add-local-user.sh
+++ Linux scripting/01-add-local-user.sh
@@ -6,6 +6,15 @@
 #13.11.23 Simple script to create user. I am using some IF statements to check if everything is alright during the creation process. There is also one example of password generation method.
 #15.11.23 I upgraded this script. So now you can only pass arguments to the script and it will create user with auto-generated password. I also provided "--help" option which will display usage.
 #19.11.23 All error messages are now displayed on standard error.Output from all other commands are suppresed.
+#23.11.23 I added function "usage" to avoid duplication in script. There are now 2 options how to print USAGE of the script. First one is with single IF statement and another one is done by getopts 
+
+usage(){
+    echo "Script to create user
+          Usage: ${0} USER_NAME [COMMENT]
+
+          OPTIONS:
+                   -h   --help   print usage of the script"
+}
 
 if [[ ${UID} -ne 0 ]]
 then
@@ -22,9 +31,23 @@ fi
 
 if [[ ${1} = '--help' ]]
 then
-   echo "Usage: ${0} USER_NAME [COMMENT]" 
+   usage
    exit 1
 fi
+
+while getopts h TESTVAR
+do
+  case ${TESTVAR} in
+    h)
+      usage
+      exit 0
+      ;;
+    ?)
+      echo "Invadil option. See '--help' OR '-h'" >&2
+      exit 1
+      ;;
+  esac
+done
 
 USER_NAME=$1
 shift
