b66129995f2fdaedfd86c005063da053b2cfb348
jakcloudlearning <jakub.cloud.learning@gmail.com>
2023-11-27 20:13:15 +0100

    Update 03-remote-command.sh

--- Linux scripting/03-remote-command.sh
+++ Linux scripting/03-remote-command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #26.11.23 I set up 3 VMs with vagrant admin1, server1, server2. I updated admin1's /etc/hosts (added mapping IP server), generated ssh-key and created file with names of servers. This script simply check connection with remote servers then it prints some basic information : hostname, status and uptime. I will add more functionality in near future.
+#27.11.23 I added new functionality. Now its a monitoring script which may be run with cron job. Output of commands is pushed into file called monitoring.log. The output is colored based on the type of message.
 
 SERVERS=$(cat /vagrant/servers)
 if [[ -z ${SERVERS} ]]
@@ -9,19 +10,18 @@ then
   exit 1 
 fi
 
-for SERVER in $SERVERS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+for IP in $SERVERS
 do
-  echo "Pinging ${SERVER}..." 
-  ping -c 1 ${SERVER} &>/dev/null
+  ping -c 1 ${IP} &>/dev/null
   if [[ "${?}" -ne 0 ]]
   then 
-     echo "${SERVER} is unreachable. The ${SERVER} may be down or there is a firewall policy to block any icmp requests."
+     echo -e "${RED}$(date +"%m-%d-%y") $(date +"%H:%M:%S") ERROR: Host ${IP} is unreachable. The host may be down or there is a firewall policy to block any icmp requests.${NC}" >> monitoring.log
   else
-     echo "${SERVER} is UP.
-           HOSTNAME:$(ssh ${SERVER} hostname)
-           UPTIME:$(ssh ${SERVER} uptime)
-           
-           "
+     echo -e "${GREEN}$(date +"%m-%d-%y") $(date +"%H:%M:%S") INFO: Host ${IP} is up. $(ssh     ${IP} hostname) $(ssh ${IP} uptime)${NC}" >> monitoring.log
   fi
 done
 
